3356d0787da49522cb28bbb6d51ca85439a442e3
Andrey <Plyakin.andrei@yandex.ru>
2022-09-14 20:24:22 +0400

    commit 2

--- ControlTask/Program.cs
+++ ControlTask/Program.cs
@@ -1,2 +1,15 @@
 ﻿string[] array = new string[6] { "Russia", "1994", "Hi", "000", "task", "1" };
 string[] resultArray = new string[array.Length];
+
+void GetArray(string[] arr, string[] arr2)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i].Length <= 3)
+        {
+            arr2[count] = arr[i];
+            count++;
+        }
+    }
+}
